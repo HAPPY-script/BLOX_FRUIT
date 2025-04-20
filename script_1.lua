39ebb055b38735a84c5e58c7d43ae68b792bdf03
HAPPY-script <happy37135535@gmail.com>
2025-04-20 23:23:44 +0700

    Create script_1.lua

---
+++ script_1.lua
@@ -0,0 +1,13 @@
+--title
+local HomeFrame = sections["Player"]
+
+local Title = Instance.new("TextLabel", HomeFrame)
+Title.Size = UDim2.new(0, 170, 0, 30)
+Title.Position = UDim2.new(0, 10, 0, 160)
+Title.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Title.TextColor3 = Color3.new(1, 1, 1)
+Title.Text = "TP KEY"
+Title.TextScaled = true
+Title.Font = Enum.Font.SourceSansBold
+Title.BorderSizePixel = 2
+Title.BorderColor3 = Color3.new(255, 255, 255)
