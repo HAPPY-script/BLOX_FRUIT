45e4f025bbe8c0c08110e476f92de01b03e7b3bf
HAPPY <happy37135535@gmail.com>
2025-04-23 23:27:02 +0700

    Update TITLE.lua

--- TITLE.lua
+++ TITLE.lua
@@ -1,7 +1,3 @@
-local FEATURE_NAME = "Title tad"
-
-local ok = pcall(function()
-
 return function(sections)
     local HomeFrame = sections["Raid"]
 
@@ -191,9 +187,6 @@ return function(sections)
     end
 end
 
---=========DEBUG===========================================================
-end)
+wait(1)
 
-if ok then
-    print(FEATURE_NAME .. " SUCCESS✅")
-end
+print(TITLE SUCCESS✅)
