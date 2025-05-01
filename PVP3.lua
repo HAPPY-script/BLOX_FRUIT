7812425b9a6d08f94b9309db6f8303a02ccb2b0f
HAPPY <happy37135535@gmail.com>
2025-05-01 11:03:59 +0700

    Update PVP3.lua

--- PVP3.lua
+++ PVP3.lua
@@ -11,8 +11,8 @@ return function(sections)
         local targetPlayer = nil
         local teleportPoints = {
             Vector3.new(-12463.61, 374.91, -7549.53), --mansion
-            Vector3.new(5661.53, 1013.04, -334.96), --women
-            Vector3.new(-5073.83, 314.51, -3152.52) --castle
+            Vector3.new(-5073.83, 314.51, -3152.52), --castle
+            Vector3.new(5661.53, 1013.04, -334.96) --women
         }
 
         -- Button bật/tắt theo dõi
