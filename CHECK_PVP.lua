79aaba1174b80d8ac9d0adaf82f120c2f763671c
HAPPY <happy37135535@gmail.com>
2025-05-01 10:12:19 +0700

    Update CHECK_PVP.lua

--- CHECK_PVP.lua
+++ CHECK_PVP.lua
@@ -3,16 +3,16 @@ return function(sections)
 
     if placeId == 2753915549 then
         -- Sea 1
-        local FruitPage1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP1.lua"))()
-        FruitPage1(sections)
+        local PVPPage1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP1.lua"))()
+        PVPPage1(sections)
     elseif placeId == 4442272183 then
         -- Sea 2
-        local FruitPage2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP2.lua"))()
-        FruitPage2(sections)
+        local PVPPage2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP2.lua"))()
+        PVPPage2(sections)
     elseif placeId == 7449423635 then
         -- Sea 3
-        local FruitPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP3.lua"))()
-        FruitPage3(sections)
+        local PVPPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP3.lua"))()
+        PVPPage3(sections)
     else
         -- Nếu không phải cả 3 Sea trên, chạy script ngoại lệ
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/NONE/refs/heads/main/NONE"))()
