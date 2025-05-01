e17d792a8ca8fd7ada4ae54c671c56a780b14ae5
HAPPY <happy37135535@gmail.com>
2025-05-01 11:27:54 +0700

    Update TP1.lua

--- TP1.lua
+++ TP1.lua
@@ -1,3 +1,18 @@
 return function(sections)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/SEA/refs/heads/main/SEA"))()
+    local HomeFrame = sections["Map"]
+
+    --===========SEA=========================================================================================
+    do
+        -- Tạo nút "Chạy Script Từ URL"
+        CreateButton(HomeFrame, "🚀 Chạy Script", 10, function()
+            local url = "https://raw.githubusercontent.com/HAPPY-script/SEA/refs/heads/main/SEA" -- Thay bằng link raw script thật của bạn
+            local success, result = pcall(function()
+                loadstring(game:HttpGet(url))()
+            end)
+            if not success then
+                warn("Không thể chạy script từ URL:", result)
+            end
+        end)
+    end
+
 end
