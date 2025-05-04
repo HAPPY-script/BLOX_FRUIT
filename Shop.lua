400d840dd15633fb2d0a6e4a5c47b51cd96b9c2a
HAPPY <happy37135535@gmail.com>
2025-05-04 13:33:18 +0700

    Create Shop.lua

---
+++ Shop.lua
@@ -0,0 +1,29 @@
+return function(sections)
+    local HomeFrame = sections["Shop"]
+
+    do
+        local btnBuyCyborg = Instance.new("TextButton", HomeFrame)
+        btnBuyCyborg.Size  = UDim2.new(0, 320, 0, 40)
+        btnBuyCyborg.Position = UDim2.new(0, 10, 0, 10)
+        btnBuyCyborg.Text  = "🤖 Buy Cyborg Race"
+        btnBuyCyborg.BackgroundColor3 = Color3.fromRGB(179, 0, 255)
+        btnBuyCyborg.TextColor3 = Color3.new(0, 0, 0)
+        btnBuyCyborg.Font = Enum.Font.SourceSansBold
+        btnBuyCyborg.TextSize = 30
+
+        btnBuyCyborg.MouseButton1Click:Connect(function()
+            local args = {
+                "CyborgTrainer",
+                "Buy"
+            }
+
+            local success, err = pcall(function()
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(args))
+            end)
+
+            if not success then
+                warn("❌ Không thể mua Cyborg Race: " .. tostring(err))
+            end
+        end)
+    end
+end
