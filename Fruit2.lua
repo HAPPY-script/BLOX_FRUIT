3c1f1b3abdacf4720535dca842a9c8c4b0959641
HAPPY <happy37135535@gmail.com>
2025-05-04 13:32:43 +0700

    Update Fruit2.lua

--- Fruit2.lua
+++ Fruit2.lua
@@ -228,7 +228,7 @@ return function(sections)
         btnRandom.BackgroundColor3 = Color3.fromRGB(255, 200, 0)
         btnRandom.TextColor3 = Color3.new(1, 1, 1)
         btnRandom.Font = Enum.Font.SourceSansBold
-        btnRandom.TextSize = 20
+        btnRandom.TextSize = 30
 
         btnRandom.MouseButton1Click:Connect(function()
             local args = {
