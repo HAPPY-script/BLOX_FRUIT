4c03942e7511c66b16bb7ee259842a2a1285d178
HAPPY <happy37135535@gmail.com>
2025-05-05 20:17:39 +0700

    Update TITLE.lua

--- TITLE.lua
+++ TITLE.lua
@@ -169,6 +169,28 @@ return function(sections)
         TitleTPButton.BorderSizePixel = 2
         TitleTPButton.BorderColor3 = Color3.new(255, 255, 255)
 
+        local TitleTPButton = Instance.new("TextLabel", HomeFrame)
+        TitleTPButton.Size = UDim2.new(0, 220, 0, 30)
+        TitleTPButton.Position = UDim2.new(0, 10, 0, 260)
+        TitleTPButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+        TitleTPButton.TextColor3 = Color3.new(1, 1, 1)
+        TitleTPButton.Text = "⚡Fast Attack Enemy🧟"
+        TitleTPButton.TextScaled = true
+        TitleTPButton.Font = Enum.Font.SourceSansBold
+        TitleTPButton.BorderSizePixel = 2
+        TitleTPButton.BorderColor3 = Color3.new(255, 255, 255)
+
+        local TitleTPButton = Instance.new("TextLabel", HomeFrame)
+        TitleTPButton.Size = UDim2.new(0, 220, 0, 30)
+        TitleTPButton.Position = UDim2.new(0, 10, 0, 310)
+        TitleTPButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+        TitleTPButton.TextColor3 = Color3.new(1, 1, 1)
+        TitleTPButton.Text = "⚡Fast Attack Player🧍"
+        TitleTPButton.TextScaled = true
+        TitleTPButton.Font = Enum.Font.SourceSansBold
+        TitleTPButton.BorderSizePixel = 2
+        TitleTPButton.BorderColor3 = Color3.new(255, 255, 255)
+
     end
 
     -- STATUS ---------------------------------------------------------
