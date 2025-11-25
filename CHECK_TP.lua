7170ba5c4f401242e52f6cc53e12f9eec0e51991
HAPPY <happy37135535@gmail.com>
2025-11-25 20:28:28 +0700

    Update CHECK_TP.lua

--- CHECK_TP.lua
+++ CHECK_TP.lua
@@ -1,5 +1,8 @@
 local sea1ID = 85211729168715
+
 local sea2ID = 79091703265657
+local sea2ID2 = 4442272183
+
 local sea3ID = 7449423635
 local sea3ID2 = 100117331123089
 
@@ -9,13 +12,10 @@ return function(sections)
     if placeId == sea1ID then
         local TPPage1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/TP1.lua"))()
         TPPage1(sections)
-    elseif placeId == sea2ID then
+    elseif placeId == sea2ID or sea2ID2 then
         local TPPage2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/TP2.lua"))()
         TPPage2(sections)
-    elseif placeId == sea3ID then
-        local TPPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/TP3.lua"))()
-        TPPage3(sections)
-    elseif placeId == sea3ID2 then
+    elseif placeId == sea3ID or sea3ID2 then
         local TPPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/TP3.lua"))()
         TPPage3(sections)
     else
