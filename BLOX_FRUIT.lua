307b608c26917da8427d4800abc9c07454f44921
HAPPY <happy37135535@gmail.com>
2025-11-25 20:25:46 +0700

    Update BLOX_FRUIT.lua

--- BLOX_FRUIT.lua
+++ BLOX_FRUIT.lua
@@ -364,12 +364,15 @@ print("✅✅Blox Fruit hub SUCCESS✅✅")
 
 -- se
 local BLOX_FRUITS_GAME_ID = 85211729168715
+
 local SECOND_SEA_GAME_ID = 79091703265657
+local SECOND_SEA_GAME_ID2 = 4442272183
+
 local THIRD_SEA_GAME_ID = 7449423635
 local THIRD_SEA_GAME_ID2 = 100117331123089
 
 local currentGameId = game.PlaceId
-if currentGameId == BLOX_FRUITS_GAME_ID or currentGameId == SECOND_SEA_GAME_ID or currentGameId == THIRD_SEA_GAME_ID or currentGameId == THIRD_SEA_GAME_ID2 then
+if currentGameId == BLOX_FRUITS_GAME_ID or currentGameId == SECOND_SEA_GAME_ID or currentGameId == SECOND_SEA_GAME_ID2 or currentGameId == THIRD_SEA_GAME_ID or currentGameId == THIRD_SEA_GAME_ID2 then
 
     local RunService = game:GetService("RunService")
     local player = game.Players.LocalPlayer
