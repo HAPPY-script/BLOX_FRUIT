da2edacfa64ef8778f15c9dba7fe6008e0d9d125
HAPPY <happy37135535@gmail.com>
2025-11-25 20:27:59 +0700

    Update CHECK_PVP.lua

--- CHECK_PVP.lua
+++ CHECK_PVP.lua
@@ -1,5 +1,8 @@
 local sea1ID = 85211729168715
+
 local sea2ID = 79091703265657
+local sea2ID2 = 4442272183
+
 local sea3ID = 7449423635
 local sea3ID2 = 100117331123089
 
@@ -8,13 +11,10 @@ return function(sections)
     if placeId == sea1ID then
         local PVPPage1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP1.lua"))()
         PVPPage1(sections)
-    elseif placeId == sea2ID then
+    elseif placeId == sea2ID or sea2ID2 then
         local PVPPage2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP2.lua"))()
         PVPPage2(sections)
-    elseif placeId == sea3ID then
-        local PVPPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP3.lua"))()
-        PVPPage3(sections)
-    elseif placeId == sea3ID2 then
+    elseif placeId == sea3ID or sea3ID2 then
         local PVPPage3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BLOX_FRUIT/refs/heads/main/PVP3.lua"))()
         PVPPage3(sections)
     else
