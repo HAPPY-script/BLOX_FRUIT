3c26d665f97fca7399ae36a882620aaf18291a7e
HAPPY <happy37135535@gmail.com>
2025-11-28 21:33:08 +0700

    Update Raid.lua

--- Raid.lua
+++ Raid.lua
@@ -291,6 +291,16 @@ return function(sections)
 
                 local island = getHighestPriorityIsland()
                 if island and not isClearingIsland then
+    
+                    -- Lọc đảo quá xa (> 5000 stud)
+                    local root = island:FindFirstChild("PrimaryPart") or island:FindFirstChildWhichIsA("BasePart")
+                    if root then
+                        local dist = (hrp.Position - root.Position).Magnitude
+                        if dist > 5000 then
+                            -- BỎ QUA đảo này, chuyển sang vòng lặp tiếp theo
+                            continue
+                        end
+                    end
 
                     -- Lấy vị trí Island
                     local root = island:FindFirstChild("PrimaryPart") or island:FindFirstChildWhichIsA("BasePart")
